ad29be15b404c9a34b2a9f1f19be68bfb06969c7
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-09-30 11:25:50 +0530

    Update develop script

--- Phase 4/develop.sh
+++ Phase 4/develop.sh
@@ -2,7 +2,7 @@
 
 if [[ "$1" == mysql ]]; then shift
     echo "Connecting to MySQL..."
-    exec docker-compose run --rm db mysql -P 3306 -h db -u root -p "$@"
+    exec docker-compose run --rm db mysql GameDhaBha -P 3306 -h db -u root -ppassword "$@"
 else
     echo "Running docker-compose with the given command..."
     exec docker-compose "$@"
