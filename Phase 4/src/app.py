cee4e0455983545a35ab2a273fd9dfff44580b4d
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-10-01 15:35:23 +0530

    Format using autopep8

--- Phase 4/src/app.py
+++ Phase 4/src/app.py
@@ -24,35 +24,42 @@ def option4():
     """
     print("Not implemented")
 
+
 def InsertVideoGame():
-    """
-    Testing whether the program works or not.
-    """
-
+    """ Inserts a new Video Game into the database """
     try:
+        # Get information about the video game
+        print("Enter new Video Game's details:")
         row = {}
-        print("Enter the new Video Game's details: ")
-        row["Name"] = input("Enter the name of the Video Game ")
-        row["ReleaseDate"] = input("Enter the release date of the VideoGame in YYYY-MM-DD format ")
-        row["LatestPatch"] = int(input("Enter the latest release number. "))
-        row["RegisteredPlayers"] = input("Enter the number of players currently playing the game. ")
-        row["OrganisationID"] = int(input("Enter the OrganisationID of the Organisation that created the game. "))
+        row["Name"] = input("Enter the name of the Video Game: ")
+        row["ReleaseDate"] = input(
+            "Enter the release date of the VideoGame in YYYY-MM-DD format: ")
+        row["LatestPatch"] = input("Enter the latest release number: ")
+        row["RegisteredPlayers"] = input(
+            "Enter the number of players currently playing the game: ")
+        row["OrganisationID"] = int(
+            input("Enter the OrganisationID of the Organisation that created the game: "))
 
-        query = f'INSERT INTO VideoGames(Name, ReleaseDate, LatestPatch, RegisteredPlayers, OrganisationID) VALUES("{row["Name"]}", "{row["ReleaseDate"]}", {row["LatestPatch"]}, {row["RegisteredPlayers"]}, {row["OrganisationID"]})'
+        # Query to be executed
+        query = f"""INSERT INTO VideoGames (Name, ReleaseDate, LatestPatch,
+                                            RegisteredPlayers, OrganisationID)
+                         VALUES ("{row["Name"]}", "{row["ReleaseDate"]}", "{row["LatestPatch"]}",
+                                 "{row["RegisteredPlayers"]}", "{row["OrganisationID"]}")
+                 """
 
+        print("\nExecuting")
         print(query)
 
+        # Execute query
         cur.execute(query)
         con.commit()
-        print("Inserted into the Database")
-    except Exception as e:
+        print("Insertion successful")
+    except Exception as error:
         con.rollback()
         print("Failed to insert into the Database")
-        print(">>>>>>>>>>>>>", e)
+        print(f"Error {error}")
 
-    return
-
-# Existing function in the given template. Keeping as of now for reference. To remove later when the project is complete. 
+# Existing function in the given template. Keeping as of now for reference. To remove later when the project is complete.
 #
 # def hireAnEmployee():
 #     """
@@ -78,22 +85,22 @@ def InsertVideoGame():
 #         row["Sex"] = input("Sex: ")
 #         row["Salary"] = float(input("Salary: "))
 #         row["Dno"] = int(input("Dno: "))
-# 
+#
 #         query = "INSERT INTO EMPLOYEE(Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Dno) VALUES('%s', '%c', '%s', '%s', '%s', '%s', '%c', %f, %d)" % (
 #             row["Fname"], row["Minit"], row["Lname"], row["Ssn"], row["Bdate"], row["Address"], row["Sex"], row["Salary"], row["Dno"])
-# 
+#
 #         print(query)
-# 
+#
 #         cur.execute(query)
 #         con.commit()
-# 
+#
 #         print("Inserted Into Database")
-# 
+#
 #     except Exception as e:
 #         con.rollback()
 #         print("Failed to insert into database")
 #         print(">>>>>>>>>>>>>", e)
-# 
+#
 #     return
 
 
@@ -119,14 +126,14 @@ while(1):
     tmp = sp.call('clear', shell=True)
 
     # Can be skipped if you want to hard core username and password
-    username = input("Username: ")
-    password = input("Password: ")
+    username = "root"
+    password = "password"
 
     try:
         # Set db name accordingly which have been create by you
         # Set host to the server's address if you don't want to use local SQL server
         con = pymysql.connect(host='localhost',
-                              port = 3306,
+                              port=3306,
                               user=username,
                               password=password,
                               db='GameDhaBha',
@@ -148,11 +155,11 @@ while(1):
                 print("2. Option 2")  # Fire an Employee
                 print("3. Option 3")  # Promote Employee
                 print("4. Option 4")  # Employee Statistics
-                print("5. Logout")
+                print("5. Quit")
                 ch = int(input("Enter choice> "))
                 tmp = sp.call('clear', shell=True)
                 if ch == 5:
-                    break
+                    exit(0)
                 else:
                     dispatch(ch)
                     tmp = input("Enter any key to CONTINUE>")
